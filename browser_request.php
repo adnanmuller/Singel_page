c82770ab936c6354a180ec44efee75300649e0b2
adnan <adnan172@orange.fr>
2019-10-29 14:05:25 +0100

    add request Info

---
+++ browser_request.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Browser Mirror</title>
+  <meta name="author" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="css/normalize.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+
+
+  <div id="main-content">
+    <h2>Browser Mirror</h2>
+          <p>Remote IP: <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
+          <p>User Agent: <?php echo $_SERVER['HTTP_USER_AGENT']; ?></p>
+          <p>Referer: <?php echo $_SERVER['HTTP_REFERER']; ?></p>
+          <p>Request Time: <?php echo $_SERVER['REQUEST_TIME']; ?></p>
+          <p>Request URI: <?php echo $_SERVER['REQUEST_URI']; ?></p>
+          <p>Request Method: <?php echo $_SERVER['REQUEST_METHOD']; ?></p>
+          <p>Query String: <?php echo $_SERVER['QUERY_STRING']; ?></p>
+          <p>HTTP Accept: <?php echo $_SERVER['HTTP_ACCEPT']; ?></p>
+          
+          <p>HTTP Accept Encoding: <?php echo $_SERVER['HTTP_ACCEPT_ENCODING']; ?></p>
+          <p>HTTP Accept Language: <?php echo $_SERVER['HTTP_ACCEPT_LANGUAGE']; ?></p>
+          <p>HTTPS?: <?php if(isset($_SERVER['HTTPS'])) {echo 'Yes'; } else {echo 'No';} ?></p>
+          <p>Remote Port: <?php echo $_SERVER['REMOTE_PORT']; ?></p>
+
+
+  </div>
+
+</body>
+
+</html>
